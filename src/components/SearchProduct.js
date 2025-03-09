943ebd20acacf0e59f1088c9d9cb34d202a0f02e
choi-mk <xhrlmin10@naver.com>
2025-03-09 19:37:48 +0900

    update search

--- src/components/SearchProduct.js
+++ src/components/SearchProduct.js
@@ -1,11 +1,19 @@
 import "./SearchProduct.css";
-export const SearchProduct = () => {
+import { useState } from "react";
+export const SearchProduct = ({ onSearch }) => {
+  const [keyword, setKeyword] = useState("");
+  const handleSearch = (e) => {
+    setKeyword(e.target.value);
+    onSearch(e.target.value);
+  };
   return (
     <div>
       <input
         className="search-product"
         type="text"
         placeholder="🔍 검색할 상품을 입력해주세요"
+        value={keyword}
+        onChange={handleSearch}
       />
     </div>
   );

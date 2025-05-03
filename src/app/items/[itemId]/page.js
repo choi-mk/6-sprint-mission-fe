1faff30657677c7bfc0b157cd6e08f6a8a24a2c0
choi-mk <xhrlmin10@naver.com>
2025-05-04 00:01:04 +0900

    목록 버튼 추가

--- src/app/items/[itemId]/page.js
+++ src/app/items/[itemId]/page.js
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import ItemDetail from "./_components/ItemDetail";
 import QuestionForm from "./_components/QuestionForm";
 import QuestionList from "./_components/QuestionList";
@@ -10,6 +11,14 @@ export default async function ItemDetailPage({ params }) {
       <ItemDetail itemId={itemId} />
       <QuestionForm itemId={itemId} />
       <QuestionList itemId={itemId} />
+      <div className="flex justify-center mt-10 ">
+        <Link href="/items">
+          <button className="bg-primary-100 text-white flex justify-center items-center w-60 h-12 font-semibold rounded-4xl">
+            목록으로 돌아가기
+            <img className="w-5 h-4" src="/assets/ic/ic_back.png" />
+          </button>
+        </Link>
+      </div>
     </div>
   );
 }
